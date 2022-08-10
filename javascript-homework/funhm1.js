cd5bab7e9320f570ec0a1ddc8943c6a5c4ded74a
abhitejarekula <109997106+abhitejarekula@users.noreply.github.com>
2022-08-10 15:26:26 -0700

    Create funhm1.js

---
+++ javascript-homework/funhm1.js
@@ -0,0 +1,44 @@
+//function age(num) {
+    //let res = num
+   // if (num >= 65) {
+  //  return ('special discount')
+  //  } else {
+       // return ('not allowed')
+  //  }
+//}
+//console.log(age(9))  // age
+
+//const n = [1 , 2 , 3]
+//const mapped = n.map(function(el) {
+    //return el * 100
+//})
+//console.log(mapped) // multiply by 100 
+
+//const n = [1, 2, 3, 4, 5 ,6 ,7]
+//const mapped = n.map(function(el,idx) {
+   // if (idx > 5) {
+       // return el * 100 } else {
+           // return el
+       // }
+  //  })
+//console.log(mapped) multiply if greater then 5
+
+//const n = [5]
+//const res = n % 2
+//const mapped = n.map(function(el) {
+    //if (res === 0) {
+      //  return el *100 
+   // } else {
+    //    return el
+   // }
+//}) 
+//console.log(mapped) // even odd
+
+// const n= [154, 657, 564, 561, 154, 678, 100, 154]
+// function mapped(array, value) {
+//     const count = 0
+//     array.forEach((v) => (v=== value && count++))
+//     }
+//     console.log(mapped(n,154))
+
+
