267e55ed4d7bee1d70f5a526b8075617be760031
abhitejarekula <109997106+abhitejarekula@users.noreply.github.com>
2022-08-09 22:29:23 -0700

    Update funhm.js

--- javascript-homework/funhm.js
+++ javascript-homework/funhm.js
@@ -1,7 +1,39 @@
-var sum = add(4, 15);
-function add (a, b) {
-    return a + b ;
-}
-console.log(sum);
+function add(num1 , num2) {
+    let result = num1 + num2
+    //return result
+//}
+//console.log(add(10, 29)) // add
 
-console.log(20 / 5);
+//function divide(num1, num2) {
+    //let result = num1 / num2
+    //return result
+//}
+//console.log(divide(20, 4)) // divide
+
+//function convert(num) {
+    //let result = (num - 32) * 5/9
+    //return result
+//}
+//console.log(convert(90)) // convert
+
+//function divisibleBy55(num) {
+    //let result = num % 55
+    //if (result === 0) {
+       // return true
+   // } else {
+      //  return false
+  //  }
+//}
+//console.log(divisibleBy55(75)) //divisibleby55
+
+
+//function checkLength(myStr) {
+    //const myStr = "wruhfwuuwviuehveuiv"
+//}
+//console.log(myStr.length) //check length
+
+//function concat(str1, str2) {
+    //let str = str1 + str2
+    //return str
+//}
+//console.log(concat(cat, dog))
