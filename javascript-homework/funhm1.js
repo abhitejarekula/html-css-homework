cce99339e44888728769544556c809db68638dd1
abhitejarekula <109997106+abhitejarekula@users.noreply.github.com>
2022-08-11 12:26:37 -0700

    Update funhm1.js

--- javascript-homework/funhm1.js
+++ javascript-homework/funhm1.js
@@ -1,27 +1,35 @@
-//function age(num) {
-    //let res = num
-   // if (num >= 65) {
-  //  return ('special discount')
-  //  } else {
+/*
+ function age(num) {
+    let res = num
+   if (num >= 65) {
+     return ('special discount')
+     } else {
        // return ('not allowed')
-  //  }
-//}
-//console.log(age(9))  // age
+     }
+ }
+*/
 
-//const n = [1 , 2 , 3]
-//const mapped = n.map(function(el) {
-    //return el * 100
-//})
-//console.log(mapped) // multiply by 100 
+/*
+console.log(age(9))  // age
 
-//const n = [1, 2, 3, 4, 5 ,6 ,7]
-//const mapped = n.map(function(el,idx) {
-   // if (idx > 5) {
-       // return el * 100 } else {
-           // return el
-       // }
-  //  })
-//console.log(mapped) multiply if greater then 5
+const n = [1 , 2 , 3]
+const mapped = n.map(function(el) {
+    return el * 100
+ })
+console.log(mapped) // multiply by 100 
+*/
+
+/*
+const n = [1, 2, 3, 4, 5]
+const mapped = n.map(function(el,idx) {
+    if (n.length > 5) { 
+        return el * 100 } 
+        else {
+            return el
+        }
+    })
+console.log(mapped) // multiply if greater then 5
+*/
 
 //const n = [5]
 //const res = n % 2
@@ -34,11 +42,32 @@
 //}) 
 //console.log(mapped) // even odd
 
-// const n= [154, 657, 564, 561, 154, 678, 100, 154]
-// function mapped(array, value) {
-//     const count = 0
-//     array.forEach((v) => (v=== value && count++))
-//     }
-//     console.log(mapped(n,154))
+/*
+let arr= [154, 657, 564, 561, 154, 678, 100, 154,154]
+let n= arr.length
+let x= 154
+function count(arr, n, x)
+{
+    let res= 0;
+    for (let i=0; i<n; i++)
+    {
+        if (x==arr[i])
+        res++;
+    }
+    return res
+}
+console.log(count(arr,n,x))
+*/
 
-
+/*
+let myVar= 78
+function checkType(myVar) {
+    let res = myVar
+    if (typeof res == 'string') {
+        return ('object')
+    } else {
+        return ('Something else')
+    }
+}
+console.log(checkType(myVar))
+*/
