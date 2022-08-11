b3cd84e080c03ace5deea0979c71be0af455427f
abhitejarekula <109997106+abhitejarekula@users.noreply.github.com>
2022-08-11 11:57:38 -0700

    Update funhm.js

--- javascript-homework/funhm.js
+++ javascript-homework/funhm.js
@@ -1,39 +1,53 @@
+/*
 function add(num1 , num2) {
     let result = num1 + num2
-    //return result
-//}
-//console.log(add(10, 29)) // add
+    return result
+}
+console.log(add(10, 29)) // add
+*/
 
-//function divide(num1, num2) {
-    //let result = num1 / num2
-    //return result
-//}
-//console.log(divide(20, 4)) // divide
+/*
+function divide(num1, num2) {
+    let result = num1 / num2
+    return result
+}
+console.log(divide(20, 4)) // divide
+*/
 
-//function convert(num) {
-    //let result = (num - 32) * 5/9
-    //return result
-//}
-//console.log(convert(90)) // convert
+/*
+function convert(num) {
+    let result = (num - 32) * 5/9
+    return result
+ }
+ console.log(convert(90)) // convert
+*/
 
-//function divisibleBy55(num) {
-    //let result = num % 55
-    //if (result === 0) {
-       // return true
-   // } else {
-      //  return false
-  //  }
-//}
-//console.log(divisibleBy55(75)) //divisibleby55
+/*
+function divisibleBy55(num) {
+    let result = num % 55
+    if (result === 0) {
+       return true
+     } else {
+       return false
+     }
+ }
+console.log(divisibleBy55(75)) //divisibleby55
+*/
+
+/*
+function checkLength(string) {
+    var len = 0;
+    len = string.length
+     return len;
+   }
+   
+   console.log(checkLength("Hello")); // 5
+   console.log(checkLength("")); // 0
+*/
 
 
-//function checkLength(myStr) {
-    //const myStr = "wruhfwuuwviuehveuiv"
-//}
-//console.log(myStr.length) //check length
-
-//function concat(str1, str2) {
-    //let str = str1 + str2
-    //return str
-//}
-//console.log(concat(cat, dog))
+function concat(str1, str2) {
+    let str = str1 + str2
+    return str
+}
+console.log(concat("cat", "dog"))
