9db77220b97de99363507e0dcd0f84c845ffd9cc
abhitejarekula <109997106+abhitejarekula@users.noreply.github.com>
2022-08-16 12:29:40 -0700

    Create testhm.js

---
+++ javascript-homework/testhm.js
@@ -0,0 +1,51 @@
+
+/*
+function largest(arr) {
+    let i
+    let max = arr[0]
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] > max)
+            max = arr[i]
+    }
+     
+  return max
+}
+let arr = [32, 47, 1, 3, 5, 50, 9, 10,4000]
+console.log(largest(arr))
+*/
+
+
+/*
+function evenodd(){
+    let arr = [ 1, 2, 3, 4, 5, 6, 7 ]
+    for (let i =0;i< arr.length;i ++) {
+        if (arr[i] % 2 === 0) {
+            console.log(arr[i])
+        }
+    }
+}
+evenodd()
+*/
+
+
+/*
+const myObject = { name: "Jack", email: "jack@gmail.com", age: 30 } 
+function loopthrough(obj) {
+    for (const property in obj) {
+        console.log(`${property}`)
+    }
+}
+loopthrough(myObject)
+*/
+
+/*
+let arr= ["h", "e", "l", "l", "o" ]
+function reverseArray(arr) {
+  let newArray = []
+  for (let i = arr.length - 1; i >= 0; i--) {
+    newArray.push(arr[i])
+  }
+  return newArray
+}
+reverseArray(arr)
+*/
